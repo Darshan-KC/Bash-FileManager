fa58616a2120c4af45e9f5ef4d67685120b89bd2
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-10-04 21:23:17 +0545

    basic structure

--- fileManager.sh
+++ fileManager.sh
@@ -1,3 +1,31 @@
 #A script that creates, renames, deletes, and moves files or directories based on user input
 
 #! /bin/bash
+
+read -p "Choose an operation: (1) Create (2) Rename (3) Delete (4) Move (5) Exit" choice
+
+case $choice in
+    1)
+        # code to create a file or directory
+        ;;
+    
+    2)
+        # code to rename a file or directory
+        ;;
+    
+    3)
+        # code to delete a file or directory
+        ;;
+    
+    4) 
+        # code to move a file or directory
+        ;;
+
+    5)
+        # Exit
+        ;;
+    
+    *)
+        echo "Invalid option. Try again."
+        ;;
+esac
